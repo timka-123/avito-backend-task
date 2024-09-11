1c459cae4e92bd05e960c81966c4bf7ae34efaa4
Timur Kh. <t1wk4.123@gmail.com>
2024-09-11 12:23:05 +0300

    add requirements + add bids module + refactor migrations

--- bids/models.py
+++ bids/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
+from core.models import User
+from tenders.models import Tender
+
+
 # Create your models here.
+
+
+class BidStatus(models.TextChoices):
+    CREATED = "Created"
+    PUBLISHED = "Published"
+    CANCELED = "Canceled"
+
+
+class BidAuthorType(models.TextChoices):
+    ORGANIZATION = "Organization"
+    USER = "User"
+
+
+class Bid(models.Model):
+    id = models.UUIDField(primary_key=True, max_length=100)
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    status = models.CharField(choices=BidStatus.choices)
+    tenderId = models.ForeignKey(Tender, on_delete=models.CASCADE)
+    authorType = models.CharField(choices=BidAuthorType.choices)
+    authorId = models.ForeignKey(User, on_delete=models.CASCADE)
+    version = models.IntegerField(default=1, db_default=1)
+    createdAt = models.DateTimeField(auto_now_add=True)
